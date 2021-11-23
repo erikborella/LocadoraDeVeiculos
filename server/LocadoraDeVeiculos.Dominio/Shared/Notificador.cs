f99c8fb225c520b6754f623f176fd85a0e57c0e6
Erik Borela <erikborella@gmail.com>
2021-11-23 16:53:12 -0300

    Criado classes notificadoras

---
+++ server/LocadoraDeVeiculos.Dominio/Shared/Notificador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraDeVeiculos.Dominio.Shared
+{
+
+    public interface INotificador
+    {
+        void RegistrarNotificacao(string notificacao);
+
+        List<string> ObterNotificacoes();
+
+        bool TemNotificacao();
+    }
+
+    public class Notificador : INotificador
+    {
+        private List<string> _notificacoes;
+
+        public void RegistrarNotificacao(string notificacao)
+        {
+            _notificacoes.Add(notificacao);
+        }
+
+        public List<string> ObterNotificacoes()
+        {
+            return _notificacoes;
+        }
+
+
+        public bool TemNotificacao()
+        {
+            return _notificacoes.Any();
+        }
+    }
+}
